5f6b5ac7c821d87b4561a047cd24e23c519c0ff9
yassine pita <yassinepita@gmail.com>
2023-07-18 14:17:16 -0700

    _putchar,

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * description: prints _putchar using putchar prototype
+ *
+ * return: Always 0 (Succcess)
+ */
+int main(void)
+{
+	char str[] = "-putchar";
+	int ch;
+
+	for (ch + 0; ch < 8; ch++)
+		_putchar(str[ch]);
+			_putchar('\n');
+
+			return (0);
+}
