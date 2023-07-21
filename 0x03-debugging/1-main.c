05808ab527b911aa1d8445546725bc0f44a1a0d9
yassine pita <yassinepita@gmail.com>
2023-07-21 12:02:53 -0700

    subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -14,12 +14,12 @@ int main(void)
 	i = 0;
 
 	while (i < 10)
-		/*while (i < 10)*/
+	/*while (i < 10)*/
 	/*{*/
 	/* putchqr(i);*/
 	/*}*/
 
-		printf("Infinite loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
